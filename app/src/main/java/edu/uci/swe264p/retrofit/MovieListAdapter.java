2ae6d72bb4f8dc7b17a1ef248cd58cf06b853834
AjCH1 <ajchen4@uci.edu>
2023-02-15 06:56:34 -0800

    Work In Progress 4
    Used Picasso to load pictures

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListAdapter.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListAdapter.java
@@ -1,5 +1,6 @@
 package edu.uci.swe264p.retrofit;
 
+import com.squareup.picasso.Picasso;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +27,7 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
     //Displays the movie
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView movieImage;
+
         TextView tvTitle;
         TextView tvReleaseDate;
         TextView tvVote;
@@ -35,11 +37,15 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
             super(itemView);
 
             movieImage = itemView.findViewById((R.id.ivMovie));
+/*
+        Picasso.get()
+                .load("https://media.geeksforgeeks.org/wp-content/cdn-uploads/logo-new-2.svg")
+                .into(holder.movieImage);
+ */
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvReleaseDate = itemView.findViewById(R.id.tvReleaseDate);
             tvVote = itemView.findViewById(R.id.tvVote);
             tvOverview = itemView.findViewById(R.id.tvOverview);
-
         }
     }
 
@@ -58,6 +64,12 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
         Movie movie = topRatedData.get(position);
 
         //ImageView
+        holder.movieImage.setImageResource(R.drawable.ic_launcher_background);
+
+        //Load from Picasso using the posterPath
+        Picasso.get()
+                .load("https://image.tmdb.org/t/p/w500" + movie.getPosterPath())
+                .into(holder.movieImage);
 
         //TextView
         holder.tvTitle.setText(movie.getTitle());
@@ -67,8 +79,8 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
     }
 
     @Override
+    //if size == 0, then the list doesn't appear in the first place
     public int getItemCount() {
-        Log.i("MOVIEROW" , String.valueOf(topRatedData.size()));
         return topRatedData.size();
     }
 }
