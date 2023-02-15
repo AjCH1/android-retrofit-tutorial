1c39e1d4319ffd6ec99a32d01bb51e2ffc22da91
AjCH1 <ajchen4@uci.edu>
2023-02-15 00:07:05 -0800

    REST call movie/top_rated

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
@@ -2,13 +2,50 @@ package edu.uci.swe264p.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MovieListActivity extends AppCompatActivity {
+    static final String TAG = MovieListActivity.class.getSimpleName();
+    static final String BASE_URL = "https://api.themoviedb.org/3/";
+    static Retrofit retrofit = null;
+    final static String API_KEY = "45a675817d425c1ee579d7f7044422e7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+    connect();
+    }
+
+    private void connect(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<Movie> call = movieApiService.getTopRatedMovies(API_KEY);
+
+        call.enqueue(new Callback<Movie>() {
+            @Override
+            public void onResponse(Call<Movie> call, Response<Movie> response) {
+                Log.i("TAG", response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<Movie> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
     }
 }
